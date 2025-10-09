c3163d2e8c7af8f74108ae59b1794ebede343e55
cernadatwit <cernad@wit.edu>
2025-10-08 20:59:34 -0400

    Completed array_fill

--- array_fill.c
+++ array_fill.c
@@ -2,7 +2,35 @@
 
 /**
  * TODO: Describe what the function does
+ * Test Cases
+ * Array Len | Begin | End | Result | Return Value
+ *      2       11      12   {11,12}        2
+ *      6       11      14   {11,12,13,14}  4
+ *      2       11      14   {11,12}        2
+ *      0       11      14      N/A         0
  */
+#include <stdlib.h>  // for malloc and free
+
 int array_fill(int* array, int array_len, int begin, int end) {
-    return 1;
+    if (array_len < 1) return 0;
+
+    if (begin>end) return 0;
+
+    int *temp = malloc(array_len * sizeof(int));
+    if (temp == NULL) return 0; // memory allocation failed
+
+    int diff = end - begin + 1;
+    int count = 0;
+    
+
+    if (diff <= array_len) {
+        for (int i = 0; i < diff; i++) {
+            temp[i] = begin++;
+            array[i] = temp[i]; // optionally copy to output array
+            count++;
+        }
+    }
+
+    free(temp);
+    return count;
 }
