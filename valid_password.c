1aa866216f5a22632abc0ea5491e9b9ada73e3c4
cernadatwit <cernad@wit.edu>
2025-10-08 21:23:49 -0400

    Completed Valid Password

--- valid_password.c
+++ valid_password.c
@@ -1,9 +1,31 @@
 #include <stddef.h> // For NULL
 #include <stdbool.h> // For true/false
+#include <ctype.h>
 
 /**
  * TODO: Describe what the function does
+ * Password must be a string 6<= s <= 10,
+ * must inlude 1 lowercase, 1 uppercase XOR 1 digit.
+ * Minimum of 2 Lowercase Letters + 2 Uppercase Letters
  */
 bool valid_password(const char* s) {
-    return true;
+    bool lowerCase2 = false;
+    bool upperCase2 = false;
+    size_t length = 0;
+    int upper = 0;
+    int lower = 0;
+    while (*s != '\0'){
+        length++;
+        if (isupper(s)){
+            upper++;
+        }
+        if (islower(s)){
+            lower++;
+        }
+        if (upper == 2 && lower == 1 && (6<=length && length<=10)){
+            return true;
+        }
+    }
+
+    return false;
 }
